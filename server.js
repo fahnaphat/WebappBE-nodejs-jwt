29c1325877bad0b2fd1a2a6b6606f687770641f5
fahnaphat <fashionironman@gmail.com>
2024-07-22 13:27:22 +0700

    update home page by ejs

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import express from "express"
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1>')
-})
+// middleware
+app.use(express.static('public'))
+
+// view engine
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(4000, () => {
     console.log('Sever started on port 4000')
