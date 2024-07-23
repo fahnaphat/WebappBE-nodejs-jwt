e06f92f4558f5403b0adaeacb34741b622f1f77b
fahnaphat <fashionironman@gmail.com>
2024-07-23 17:18:32 +0700

    connecting to db and install dotenv

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 
+dotenv.config({ path: './.env' })
 const app = express()
+
+const port = process.env.PORT || 5500
 
 // middleware
 app.use(express.static('public'))
@@ -9,9 +13,23 @@ app.use(express.static('public'))
 // view engine
 app.set('view engine', 'ejs')
 
+// database connection
+const dbURI = process.env.DATABASE_MONGO
+
+async function connectdb() {
+    try {
+        await mongoose.connect(dbURI)
+        console.log('Connected to Mongodb')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connectdb();
+
 app.get('/', (req, res) => res.render('home'))
 app.get('/post', (req, res) => res.render('post'))
 
-app.listen(4000, () => {
-    console.log('Sever started on port 4000')
+app.listen((port), () => {
+    console.log('Server started on port',port)
 })
