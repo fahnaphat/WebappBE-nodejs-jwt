9a64969087c3ab452834fab75eeac6b42dd0b8eb
fahnaphat <fashionironman@gmail.com>
2024-07-23 18:27:23 +0700

    create siginup/login with post, get api

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+export const signup_get = (req, res) => {
+    res.render('register')
+}
+
+export const signup_post = (req, res) => {
+    res.send('new signup')
+}
+
+export const login_get = (req, res) => {
+    res.render('login')
+}
+
+export const login_post = (req, res) => {
+    res.send('user login')
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+import authRoutes from './routes/authRoutes.js'
+
 dotenv.config({ path: './.env' })
 const app = express()
 
@@ -9,13 +11,13 @@ const port = process.env.PORT || 5500
 
 // middleware
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true })) // to parse form data
 
 // view engine
 app.set('view engine', 'ejs')
 
 // database connection
 const dbURI = process.env.DATABASE_MONGO
-
 async function connectdb() {
     try {
         await mongoose.connect(dbURI)
@@ -24,11 +26,15 @@ async function connectdb() {
         console.log(error)
     }
 }
-
-connectdb();
+// connectdb();
 
 app.get('/', (req, res) => res.render('home'))
 app.get('/post', (req, res) => res.render('post'))
+// app.use((req, res, next) => {
+//     console.log(`Request URL: ${req.url}, Method: ${req.method}`)
+//     next()
+// })
+app.use(authRoutes)
 
 app.listen((port), () => {
     console.log('Server started on port',port)
