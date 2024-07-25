f5d37bc66b14e84b025fc213481dfe4875eb4731
fahnaphat <fashionironman@gmail.com>
2024-07-25 22:14:43 +0700

    handle errors for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,18 @@ import jwt from "jsonwebtoken";
 
 // handle errors
 const handleErrors = (err) => {
-    // console.log(err.message, err.code)
+    console.log(err.message, err.code)
     let errors = { firstname: '', lastname: '', email: '', password: '' }
+
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'email is not registered'
+    }
+    
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'password is incorrect'
+    }
 
     // duplicate error code
     if (err.code === 11000) {
@@ -63,9 +73,12 @@ export const login_post = async (req, res) => {
 
     try {
         const user = await User.login(email, password)
+        const token = createToken(user._id)
+        res.cookie('jwt', token, { httpOnly: true, maxAge: expireDate * 1000 })
         res.status(200).json({ user: user._id })
     }
     catch (err) {
-        res.status(400).json({})
+        const errors = handleErrors(err)
+        res.status(400).json({ errors })
     }
 }
